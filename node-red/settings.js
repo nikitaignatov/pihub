8c8208059644e71faf3dcc40ff757b095ffd6e07
Nikita Ignatov <4703715+nikitaignatov@users.noreply.github.com>
2019-06-18 02:19:16 +0200

    Update settings.js

--- node-red/settings.js
+++ node-red/settings.js
@@ -16,13 +16,6 @@ module.exports = {
     },
     logging: {
         console: {
-            // fatal - only those errors which make the application unusable should be recorded
-            // error - record errors which are deemed fatal for a particular request + fatal errors
-            // warn - record problems which are non fatal + errors + fatal errors
-            // info - record information about the general running of the application + warn + error + fatal errors
-            // debug - record information which is more verbose than info + info + warn + error + fatal errors
-            // trace - record very detailed logging + debug + info + warn + error + fatal errors
-            // off - turn off all logging (doesn't affect metrics or audit)
             level: "info",
             metrics: false,
             audit: false
