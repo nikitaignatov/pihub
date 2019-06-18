44d5728c4a96b7830eee3220f4846a246e8c701c
Nikita Ignatov <4703715+nikitaignatov@users.noreply.github.com>
2019-06-18 20:08:08 +0200

    Create usb_mount_entrypoint.sh

---
+++ influxdb/usb_mount_entrypoint.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+mkdir -p /mnt/usb
+mount /mnt/usb
+
+set -e
+
+if [ "${1:0:1}" = '-' ]; then
+    set -- influxd "$@"
+fi
+
+if [ "$1" = 'influxd' ]; then
+        /init-influxdb.sh "${@:2}"
+fi
+
+exec "$@"
